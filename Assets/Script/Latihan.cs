060fcd4b2baa17ed8a9fc35e3d4f859fd323ccaa
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-11 09:53:15 +0700

    Update Latihan.cs

--- Assets/Script/Latihan.cs
+++ Assets/Script/Latihan.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Latihan : MonoBehaviour
@@ -5,7 +6,7 @@ public class Latihan : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cobaWhile(10);
+        Debug.Log(factorial(4));
     }
 
     // Update is called once per frame
@@ -13,15 +14,51 @@ public class Latihan : MonoBehaviour
     {
 
     }
-    
+
     void cobaWhile(int nyawa)
     {
         Debug.Log("Awal nyawa : " + nyawa);
-        while(nyawa > 0)
+        while (nyawa > 0)
         {
             nyawa -= 1;
             Debug.Log(nyawa);
         }
+    }
+
+    void cobaFor(int hit)
+    {
+        for (int i = 1; i <= hit; i++)
+        {
+            Debug.Log("Pukulan ke : " + i);
+        }
+    }
+
+    int factorial(int angka)
+    {
+        int hasil = angka;
+        String penjabaran = angka + "! = 4 x ";
+        for (int i = angka - 1; i > 0; i--)
+        {
+            penjabaran += i + " x ";
+            hasil = hasil * i;
+        }
+        Debug.Log(penjabaran);
+        return hasil;
+    }
+    
+    void segitiga(int tinggi)
+    {
+        String hasil = "";
+        for (int i = 1; i <= tinggi; i++)
+        {
+            for (int a = 1; a <= i; a++)
+            {
+                hasil += "* ";
+            }
+            hasil += "\n";
+        }
+
+        Debug.Log(hasil);
     }
 }
 
