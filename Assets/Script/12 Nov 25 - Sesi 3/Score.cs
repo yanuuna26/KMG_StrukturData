2e327e4e45e292438e417e3c41b69c39b02cd0c7
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-12 09:55:48 +0700

    Invocation Method

--- Assets/Script/12 Nov 25 - Sesi 3/Score.cs
+++ Assets/Script/12 Nov 25 - Sesi 3/Score.cs
@@ -2,23 +2,22 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-
-    [SerializeField]
-    int score;
-    DeteksiInput objectDeteksi;
+    public int score;
+    public DeteksiInput objectDeteksi;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
         objectDeteksi.inputEvent += AddScore;
     }
 
+    public void AddScore()
+    {
+        score++;
+    }
     void OnDisable()
     {
         objectDeteksi.inputEvent -= AddScore;
     }
 
-    public void AddScore()
-    {
-        score++;
-    }
+    
 }
